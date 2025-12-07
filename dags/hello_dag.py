8018248eb7d6372fcd456c493612c324f4e19157
abcdefya <anh.dothe47@gmail.com>
2025-12-08 02:04:18 +0700

    hello dag from artifact registry image

--- dags/hello_dag.py
+++ dags/hello_dag.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
 default_args = {
     'owner': 'airflow',
@@ -11,16 +11,21 @@ default_args = {
 with DAG(
     dag_id='hello_world_dag',
     default_args=default_args,
-    description='A simple Hello World DAG',
+    description='A simple Hello World DAG using KubernetesPodOperator',
     start_date=datetime(2023, 1, 1),
     schedule='@daily',
     catchup=False,
 ) as dag:
 
-    hello_task = BashOperator(
+    hello_task = KubernetesPodOperator(
         task_id='hello_task',
-        bash_command='echo "Hello World!"',
+        name='hello-task',
+        namespace='airflow',
+        image='asia-southeast1-docker.pkg.dev/binance-test-479915/bnb-c2c-images/hello-python:v1',
+        cmds=['python', '-c', 'print("Hello World from K8s!")'],
+        image_pull_policy='Always',
+        is_delete_operator_pod=True,
+        get_logs=True,
     )
 
     hello_task
-
