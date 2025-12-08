a352280c229e0a71ab6ce61b5858b99f51d8d7e9
abcdefya <anh.dothe47@gmail.com>
2025-12-08 09:24:40 +0700

    added namespace to dag

--- dags/hello_dag.py
+++ dags/hello_dag.py
@@ -20,7 +20,7 @@ with DAG(
     hello_task = KubernetesPodOperator(
         task_id='hello_task',
         name='hello-task',
-        namespace='airflow',
+        namespace='orchestration',
         image='asia-southeast1-docker.pkg.dev/binance-test-479915/bnb-c2c-images/hello-python:v1',
         cmds=['python', '-c', 'print("Hello World from K8s!")'],
         image_pull_policy='Always',
